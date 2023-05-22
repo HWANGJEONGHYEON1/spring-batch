90589fc242b18606616264b9e34a9b422fe6d0eb
jhhwnag <jhhwang@wemakeprice.com>
2023-05-22 13:40:36 +0900

    study: spring-batch basic

---
+++ src/main/java/com/example/springbatch/job/SimpleJobConfiguration.java
@@ -0,0 +1,62 @@
+package com.example.springbatch.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class SimpleJobConfiguration {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job simpleJob() {
+        return jobBuilderFactory.get("stepNextJob")
+                .start(simpleStep1())
+                .next(simpleStep2())
+                .next(simpleStep3())
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step1");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step simpleStep2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step2");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @JobScope
+    public Step simpleStep3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>> This is Step3");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+}
---
+++ src/main/java/com/example/springbatch/job/StepNextConditionalJobConfiguration.java
@@ -0,0 +1,66 @@
+package com.example.springbatch.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class StepNextConditionalJobConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job stepNextConditionalJob() {
+        return jobBuilderFactory.get("stepNextConditionalJob")
+                .start(conditionalJobStep1())
+                    .on("FAILED")// FAILED 일 경우
+                    .to(conditionalJobStep3())// step3으로 이동
+                    .on("*")// step3의 결과와 상관없이
+                    .end() // step3으로 이동하면 Flow가 종료된다.
+                .from(conditionalJobStep1()) // step1로부터
+                    .on("*")// FAILED 외 모든 경우에
+                    .to(conditionalJobStep2()) // step2 이동
+                    .next(conditionalJobStep3()) // step2가 완료되면 step3
+                    .on("*") // step3 결과에 상관없이
+                    .end() // 종료
+                .end().build(); // job 종료
+    }
+
+    @Bean
+    public Step conditionalJobStep1() {
+        return stepBuilderFactory.get("step1")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>> this is step1");
+                    contribution.setExitStatus(ExitStatus.FAILED);
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Step conditionalJobStep2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>> this is step2");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+
+    @Bean
+    public Step conditionalJobStep3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>> this is step3");
+                    return RepeatStatus.FINISHED;
+                }).build();
+    }
+}
